ca120b85c531a51154ea45050b93fffc18502217
bharat.mahajan <bharat.mahajan@amebasoftwares.com>
2017-10-07 01:27:33 +0530

    First Commit of OlaFunLola project

---
+++ OlaFunMiLola/Property/Admin/PropertyVideo.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Data;
+using Property;
+using Property_cls;
+
+namespace Property.Admin
+{
+    public partial class PropertyVideo : System.Web.UI.Page
+    {
+        #region Global
+        cls_Property clsobj = new cls_Property();
+        #endregion Global
+
+        #region Page Load
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (Session["FirstName"] != null)
+            //{
+            if (!IsPostBack)
+            {
+                FillGridData();
+            }
+            //}
+            //else
+            //{
+            //    Response.Redirect("~/Admin/AdminLogin.aspx", false);
+            //}
+        }
+
+        #endregion Page Load
+
+        #region PageListGrid Events & Method
+
+        protected void FillGridData()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                dt = clsobj.GetVideoList();
+                grdBannerShow.DataSource = dt;
+                grdBannerShow.DataBind();
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+
+        protected void GrdBlogList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int id = 0;
+            if (e.CommandName == "Deleterec")
+            {
+                id = Convert.ToInt32(e.CommandArgument);
+                int result = clsobj.DeleteVideo(id);
+                FillGridData();
+            }
+            else if (e.CommandName == "Editrec")
+            {
+                id = Convert.ToInt32(e.CommandArgument);
+                clsobj.ChangeVideo(id);
+                
+                //imgbanner.Visible = true;
+                FillGridData();
+            }
+            
+            else
+            {
+                FillGridData();
+            }
+        }
+
+
+
+
+
+        #endregion PageListGrid Events & Method
+
+        #region Button Click
+
+        protected void btnUploadImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] bytes = null;
+                
+                imgbanner.Visible = false;
+                HttpFileCollection fileCollection = Request.Files;
+                HttpPostedFile uploadfileSingle = fileCollection[0];
+                var videoLink=  SaveFile(uploadfileSingle);
+                if (videoLink != null)
+                {
+                    clsobj.InsertPropertyVideo(videoLink, "video/mp4", bytes, videoLink);
+                }
+                FillGridData();
+            }
+            catch (Exception ex)
+            {
+                //ErrorLogging.WriteLog(ex.ToString());
+            }
+
+        }
+        public string SaveFile(HttpPostedFile file)
+        {
+            //if (PhotoPath != "")
+            //{   //Delete Old Image
+            //    string pathDel = Server.MapPath("~/CustomerPhoto");
+
+            //    FileInfo objfile = new FileInfo(pathDel);
+            //    if (objfile.Exists) //check file exsit or not
+            //    {
+            //        objfile.Delete();
+            //    }
+            //    //End :Delete Old Image
+            //}
+
+            //Save the photo in Folder
+            var fileExt = Path.GetExtension(file.FileName);
+            string fileName = Guid.NewGuid() + fileExt;
+            var subPath = Server.MapPath("~/uploadfiles");
+
+            //Check SubPath Exist or Not
+            if (!Directory.Exists(subPath))
+            {
+                Directory.CreateDirectory(subPath);
+            }
+            //End : Check SubPath Exist or Not
+
+            var path = Path.Combine(subPath, fileName);
+            file.SaveAs(path);
+
+            return "http://localhost:61760/uploadfiles/" + fileName;
+        }
+        #endregion Button Click
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+           // txtutube.Text = "";
+            //txtDescription.Text = "";
+            //imgbanner.Visible = false;
+        }
+    }
+}
---
+++ OlaFunMiLola/Property/VipAccess.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Net.Mail;
+using System.Net;
+using Property_cls;
+using Property_Cryptography;
+
+namespace Property
+{
+    public partial class VipAccess : System.Web.UI.Page
+    {
+
+        #region Global
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ToString());
+
+        #endregion Global
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // CalendarExtender1.StartDate = DateTime.Now;
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string email = ConfigurationManager.AppSettings["RegFromMailAddress"];
+                //string toemail = ConfigurationManager.AppSettings["MailToAddress"];
+                MailMessage message = new MailMessage();
+                message.To.Add(txtEmail.Text);
+                message.From = new MailAddress(ConfigurationManager.AppSettings["RegFromMailAddress"]);
+                message.Subject = "You are registered for VIP Aceess";
+                message.Body = "Name:" + txtFirstName.Text + "<br/>" + "You are sucessfully register for VIP Access.";
+                message.IsBodyHtml = true;
+                SmtpClient smtpClient = new SmtpClient(ConfigurationManager.AppSettings["SmtpServer"]);
+                smtpClient.Port = Int32.Parse(ConfigurationManager.AppSettings["Port"]);
+                smtpClient.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["RegFromMailAddress"], ConfigurationManager.AppSettings["RegPassword"]);
+                smtpClient.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]);
+                smtpClient.Send(message);
+                clearform();
+                //dvshw.Style["display"] = "show";
+                //lblmessage.Text = "Thanks For Sending Message!";
+                conn.Close();
+                if ((Session["FeatureType"]) != null)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "ScriptKey", "alert('Your Appointment  has been saved');window.location='featureListing.aspx'; ", true);
+                    //   Response.Redirect("~/featureListing.aspx", false);
+
+                }
+                else if (Session["Municipality"] != null)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "click", "alert('Your Appointment  has been saved');window.location.href ='Search.aspx?Municipality=" + Convert.ToString(Session["Municipality"]) + "'", true);
+                    //  string targeturl=  "~/Search.aspx?Municipality=" + Convert.ToString(Session["Municipality"]);
+                }
+                else if (Session["Type1"] != null)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "click", "alert('Your Appointment  has been saved..');window.location.href ='PropertyDetails.aspx?MLSID=" + Convert.ToString(Session["MLSID"]) + "&PropertyType=" + Convert.ToString(Session["Type1"]) + "'", true);
+                    //     Response.Redirect("PropertyDetails.aspx?MLSID=" + Convert.ToString(Session["MLSID"]) + "&PropertyType=" + Convert.ToString(Session["Type1"]), false);
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "ScriptKey", "alert('Your Appointment  has been saved');window.location='ScheduleAppointment.aspx'; ", true);
+                    //   Response.Redirect("~/Search.aspx", false);
+                }
+            }
+            catch (Exception ex)
+            {
+                lblmsg.Text = "An error occurred!!Try again";
+                // ErrorMessege.Text = "An error occurred!!";
+            }
+        }
+        private void clearform()
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtPhoneNo.Text = "";
+            ProjectName.Text = "";
+            txtNotes.Text = "";
+        }
+
+    }
+}
